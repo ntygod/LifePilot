41d26ff644bc6ef2a849b34345aaca03005f0a86
金陵雪 <863401367@qq.com>
2026-03-16 16:10:22 +0800

    feat(meta): 新增 ScrollResult/TabInfo/AccessibilityNode 数据模型

---
+++ src/main/java/com/lifepilot/meta/infra/browser/ScrollResult.java
@@ -0,0 +1,9 @@
+package com.lifepilot.meta.infra.browser;
+
+/**
+ * 滚动结果。
+ *
+ * @author zsg
+ * @since 2026-03-16
+ */
+public record ScrollResult(double scrollX, double scrollY) {}
---
+++ src/main/java/com/lifepilot/meta/infra/browser/TabInfo.java
@@ -0,0 +1,9 @@
+package com.lifepilot.meta.infra.browser;
+
+/**
+ * 标签页信息。
+ *
+ * @author zsg
+ * @since 2026-03-16
+ */
+public record TabInfo(String tabId, String url, String title) {}
