720ca8e586c857139c46429b6660fb5c41f0a2e0
金陵雪 <863401367@qq.com>
2026-03-18 20:55:47 +0800

    feat(memory): 注册经验学习增强组件 Bean

--- src/main/java/com/lifepilot/memory/config/MemoryAutoConfiguration.java
+++ src/main/java/com/lifepilot/memory/config/MemoryAutoConfiguration.java
@@ -653,6 +653,62 @@ public class MemoryAutoConfiguration {
                 promptRegistry, properties, qualityAssessor, evalStore);
     }
 
+    // --- 经验学习增强 ---
+
+    @Bean
+    @ConditionalOnMissingBean
+    @ConditionalOnBean(SemanticMemory.class)
+    public com.lifepilot.memory.experience.EffectivenessTracker effectivenessTracker(
+            SemanticMemory semanticMemory,
+            InjectionRecordRepository injectionRecordRepository,
+            MemoryProperties properties) {
+        log.info("记忆系统: 注册 EffectivenessTracker");
+        return new com.lifepilot.memory.experience.EffectivenessTracker(
+                semanticMemory, injectionRecordRepository, properties);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    @ConditionalOnBean({SemanticMemory.class, VectorSearcher.class})
+    public com.lifepilot.memory.experience.ContrastiveLearner contrastiveLearner(
+            SemanticMemory semanticMemory,
+            VectorSearcher vectorSearcher,
+            LlmRouter llmRouter,
+            PromptRegistry promptRegistry,
+            MemoryProperties properties) {
+        log.info("记忆系统: 注册 ContrastiveLearner");
+        return new com.lifepilot.memory.experience.ContrastiveLearner(
+                semanticMemory, vectorSearcher, llmRouter, promptRegistry, properties);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    @ConditionalOnBean({SemanticMemory.class, VectorSearcher.class})
+    public com.lifepilot.memory.experience.SubtaskReflector subtaskReflector(
+            SemanticMemory semanticMemory,
+            VectorSearcher vectorSearcher,
+            LlmRouter llmRouter,
+            PromptRegistry promptRegistry,
+            MemoryProperties properties) {
+        log.info("记忆系统: 注册 SubtaskReflector");
+        return new com.lifepilot.memory.experience.SubtaskReflector(
+                semanticMemory, vectorSearcher, llmRouter, promptRegistry, properties);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    @ConditionalOnBean({SemanticMemory.class, VectorSearcher.class})
+    public com.lifepilot.memory.consolidation.ExperienceMerger experienceMerger(
+            SemanticMemory semanticMemory,
+            VectorSearcher vectorSearcher,
+            LlmRouter llmRouter,
+            PromptRegistry promptRegistry,
+            MemoryProperties properties) {
+        log.info("记忆系统: 注册 ExperienceMerger");
+        return new com.lifepilot.memory.consolidation.ExperienceMerger(
+                semanticMemory, vectorSearcher, llmRouter, promptRegistry, properties);
+    }
+
     // --- 工具方法 ---
 
     /** 检测 sqlite-vec 扩展是否可用。 */
