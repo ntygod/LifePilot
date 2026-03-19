870dbf461d2e61c2f3459a3f9d98a903b5358e77
金陵雪 <863401367@qq.com>
2026-03-19 22:42:43 +0800

    refactor(skill): 删除待办/日程/习惯内置 Skill 及相关配置

--- src/main/java/com/lifepilot/skill/builtin/habit/HabitLog.java
+++
@@ -1,9 +0,0 @@
-package com.lifepilot.skill.builtin.habit;
-
-/**
- * 打卡记录。
- *
- * @author zsg
- * @since 2026-02-25
- */
-public record HabitLog(String id, String habitId, String checkedAt, String createdAt) {}
